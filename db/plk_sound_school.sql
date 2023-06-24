afc9b754805330fe7df7d42682bc39b6b4b8321e
Monoget <monoget2@gmail.com>
2023-06-24 18:09:19 +0600

    Update files

--- db/plk_sound_school.sql
+++ db/plk_sound_school.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 25, 2023 at 07:53 PM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.2.0
+-- Generation Time: Jun 24, 2023 at 02:08 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,6 +24,31 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `admin_login`
+--
+
+CREATE TABLE `admin_login` (
+  `id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `ip` varchar(150) NOT NULL,
+  `image` varchar(100) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `role` varchar(15) NOT NULL DEFAULT 'sales',
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admin_login`
+--
+
+INSERT INTO `admin_login` (`id`, `name`, `ip`, `image`, `email`, `password`, `role`, `updated_at`) VALUES
+(1, 'Monoget Saha', '27.147.190.199', 'public/images/profile/monoget.png', 'monoget1@gmail.com', '@BCD1234', 'admin', '2022-02-06 11:16:17'),
+(2, 'Super Admin', '103.107.160.134', 'public/images/avatar-01.jpg', 'admin@school.com', '@BCD1234', 'admin', '2023-06-24 11:58:33');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `sound`
 --
 
@@ -33,7 +58,7 @@ CREATE TABLE `sound` (
   `user_id` int(11) NOT NULL,
   `play` int(11) NOT NULL DEFAULT 1,
   `inserted_at` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -44,38 +69,39 @@ CREATE TABLE `sound` (
 CREATE TABLE `sound_list` (
   `id` int(11) NOT NULL,
   `code` varchar(20) NOT NULL,
+  `sound_name` varchar(100) NOT NULL,
   `inserted_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `sound_list`
 --
 
-INSERT INTO `sound_list` (`id`, `code`, `inserted_at`) VALUES
-(1, 'MAS1', '2023-04-25 17:50:36'),
-(2, 'RIS1', '2023-04-25 17:50:58'),
-(3, 'ELV1', '2023-04-25 17:53:35'),
-(4, 'EUP1', '2023-04-25 17:53:35'),
-(5, 'WEF2', '2023-04-25 17:53:35'),
-(6, 'WEY2', '2023-04-25 17:53:35'),
-(7, 'ANE2', '2023-04-25 17:53:35'),
-(8, 'MAP2', '2023-04-25 17:53:35'),
-(9, 'JOK3', '2023-04-25 17:53:35'),
-(10, 'MIE3', '2023-04-25 17:53:35'),
-(11, 'JAA3', '2023-04-25 17:53:35'),
-(12, 'AMM3', '2023-04-25 17:53:35'),
-(13, 'FAM4', '2023-04-25 17:53:35'),
-(14, 'JAR4', '2023-04-25 17:53:35'),
-(15, 'STZ4', '2023-04-25 17:53:35'),
-(16, 'DAL4', '2023-04-25 17:53:35'),
-(17, 'KEJ5', '2023-04-25 17:53:35'),
-(18, 'ASK5', '2023-04-25 17:53:35'),
-(19, 'AMC5', '2023-04-25 17:53:35'),
-(20, 'ALV5', '2023-04-25 17:53:35'),
-(21, 'CHV6', '2023-04-25 17:53:35'),
-(22, 'JUL6', '2023-04-25 17:53:35'),
-(23, 'MIL6', '2023-04-25 17:53:35'),
-(24, 'BEB6', '2023-04-25 17:53:35');
+INSERT INTO `sound_list` (`id`, `code`, `sound_name`, `inserted_at`) VALUES
+(1, 'MAS1', 'assets/audio/MAS1.wav', '2023-06-24 12:03:42'),
+(2, 'RIS1', 'assets/audio/RIS1.wav', '2023-06-24 12:03:42'),
+(3, 'ELV1', 'assets/audio/ELV1.wav', '2023-06-24 12:03:42'),
+(4, 'EUP1', 'assets/audio/EUP1.wav', '2023-06-24 12:03:42'),
+(5, 'WEF2', 'assets/audio/WEF2.wav', '2023-06-24 12:03:42'),
+(6, 'WEY2', 'assets/audio/WEY2.wav', '2023-06-24 12:03:42'),
+(7, 'ANE2', 'assets/audio/ANE2.wav', '2023-06-24 12:03:42'),
+(8, 'MAP2', 'assets/audio/MAP2.wav', '2023-06-24 12:03:42'),
+(9, 'JOK3', 'assets/audio/JOK3.wav', '2023-06-24 12:03:42'),
+(10, 'MIE3', 'assets/audio/MIE3.wav', '2023-06-24 12:03:42'),
+(11, 'JAA3', 'assets/audio/JAA3.wav', '2023-06-24 12:03:42'),
+(12, 'AMM3', 'assets/audio/AMM3.wav', '2023-06-24 12:03:42'),
+(13, 'FAM4', 'assets/audio/FAM4.wav', '2023-06-24 12:03:42'),
+(14, 'JAR4', 'assets/audio/JAR4.wav', '2023-06-24 12:03:42'),
+(15, 'STZ4', 'assets/audio/STZ4.wav', '2023-06-24 12:03:42'),
+(16, 'DAL4', 'assets/audio/DAL4.wav', '2023-06-24 12:03:42'),
+(17, 'KEJ5', 'assets/audio/KEJ5.wav', '2023-06-24 12:03:42'),
+(18, 'ASK5', 'assets/audio/ASK5.wav', '2023-06-24 12:03:42'),
+(19, 'AMC5', 'assets/audio/AMC5.wav', '2023-06-24 12:03:42'),
+(20, 'ALV5', 'assets/audio/ALV5.wav', '2023-06-24 12:03:42'),
+(21, 'CHV6', 'assets/audio/CHV6.wav', '2023-06-24 12:03:42'),
+(22, 'JUL6', 'assets/audio/JUL6.wav', '2023-06-24 12:03:42'),
+(23, 'MIL6', 'assets/audio/MIL6.wav', '2023-06-24 12:03:42'),
+(24, 'BEB6', 'assets/audio/BEB6.wav', '2023-06-24 12:03:42');
 
 -- --------------------------------------------------------
 
@@ -87,11 +113,48 @@ CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `ip` varchar(20) NOT NULL,
   `inserted_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `ip`, `inserted_at`) VALUES
+(1, '::1', '2023-04-26 11:05:51'),
+(2, '::1', '2023-04-26 11:06:01'),
+(3, '::1', '2023-04-26 11:26:39'),
+(4, '::1', '2023-04-26 11:26:41'),
+(5, '127.0.0.1', '2023-04-26 11:27:25'),
+(6, '127.0.0.1', '2023-04-26 11:29:06'),
+(7, '::1', '2023-04-26 11:29:09'),
+(8, '127.0.0.1', '2023-04-26 11:29:38'),
+(9, '127.0.0.1', '2023-04-26 11:31:23'),
+(10, '127.0.0.1', '2023-04-26 11:32:06'),
+(11, '127.0.0.1', '2023-04-26 11:33:01'),
+(12, '127.0.0.1', '2023-04-26 11:34:56'),
+(13, '::1', '2023-04-26 11:35:25'),
+(14, '::1', '2023-04-26 11:37:43'),
+(15, '::1', '2023-04-26 11:37:45'),
+(16, '127.0.0.1', '2023-06-24 15:36:04'),
+(17, '127.0.0.1', '2023-06-24 15:36:09'),
+(18, '127.0.0.1', '2023-06-24 16:34:06'),
+(19, '127.0.0.1', '2023-06-24 16:34:19'),
+(20, '127.0.0.1', '2023-06-24 16:36:10'),
+(21, '127.0.0.1', '2023-06-24 16:36:20'),
+(22, '127.0.0.1', '2023-06-24 16:37:37'),
+(23, '127.0.0.1', '2023-06-24 16:40:50'),
+(24, '127.0.0.1', '2023-06-24 16:41:36'),
+(25, '127.0.0.1', '2023-06-24 17:24:50');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admin_login`
+--
+ALTER TABLE `admin_login`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `sound`
@@ -116,6 +179,12 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `admin_login`
+--
+ALTER TABLE `admin_login`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
 -- AUTO_INCREMENT for table `sound`
 --
 ALTER TABLE `sound`
@@ -131,7 +200,7 @@ ALTER TABLE `sound_list`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
