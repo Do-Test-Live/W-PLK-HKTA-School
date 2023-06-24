567d4d1d1c931ead8de35687d44a9dd1e1ef9e5a
Monoget <monoget2@gmail.com>
2023-06-24 17:23:44 +0600

    update site

--- index.php
+++ index.php
@@ -67,130 +67,34 @@ $_SESSION['user_id'] = $data[0]['id'];
 <section>
     <div class="container-fluid">
         <div class="row text-center">
-            <div class="col-lg-6">
-                <div class="row">
-                    <div class="col-xl-3 col-md-4 col-6 mb-4">
-                        <button class="btn btn-primary hkta-primary-btn" id="MAS1" type="button">MAS1</button>
-                    </div>
-                    <div class="col-xl-3 col-md-4 col-6 mb-4">
-                        <button class="btn btn-primary hkta-primary-btn" id="RIS1" type="button">RIS1</button>
-                    </div>
-                    <div class="col-xl-3 col-md-4 col-6 mb-4">
-                        <button class="btn btn-primary hkta-primary-btn" id="ELV1" type="button">ELV1</button>
-                    </div>
-                    <div class="col-xl-3 col-md-4 col-6 mb-4">
-                        <button class="btn btn-primary hkta-primary-btn" id="EUP1" type="button">EUP1</button>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-6">
-                <div class="row">
-                    <div class="col-xl-3 col-md-4 col-6 mb-4">
-                        <button class="btn btn-primary hkta-primary-btn" id="WEF2" type="button">WEF2</button>
-                    </div>
-                    <div class="col-xl-3 col-md-4 col-6 mb-4">
-                        <button class="btn btn-primary hkta-primary-btn" id="WEY2" type="button">WEY2</button>
-                    </div>
-                    <div class="col-xl-3 col-md-4 col-6 mb-4">
-                        <button class="btn btn-primary hkta-primary-btn" id="ANE2" type="button">ANE2</button>
-                    </div>
-                    <div class="col-xl-3 col-md-4 col-6 mb-4">
-                        <button class="btn btn-primary hkta-primary-btn" id="MAP2" type="button">MAP2</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+            <?php
+            $query = "SELECT * FROM sound_list order by id asc";
+            $sound_data = $db_handle->runQuery($query);
+            $sound_row = $db_handle->numRows($query);
 
-    <div class="container-fluid">
-        <div class="row text-center">
-            <div class="col-lg-6">
-                <div class="row">
-                    <div class="col-xl-3 col-md-4 col-6 mb-4">
-                        <button class="btn btn-primary hkta-primary-btn" id="JOK3" type="button">JOK3
-                        </button>
-                    </div>
-                    <div class="col-xl-3 col-md-4 col-6 mb-4">
-                        <button class="btn btn-primary hkta-primary-btn" id="MIE3" type="button">MIE3
-                        </button>
-                    </div>
-                    <div class="col-xl-3 col-md-4 col-6 mb-4">
-                        <button class="btn btn-primary hkta-primary-btn" id="JAA3" type="button">JAA3
-                        </button>
-                    </div>
-                    <div class="col-xl-3 col-md-4 col-6 mb-4">
-                        <button class="btn btn-primary hkta-primary-btn" id="AMM3" type="button">AMM3
-                        </button>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-6">
-                <div class="row">
-                    <div class="col-xl-3 col-md-4 col-6 mb-4">
-                        <button class="btn btn-primary hkta-primary-btn" id="FAM4" type="button">FAM4
-                        </button>
-                    </div>
-                    <div class="col-xl-3 col-md-4 col-6 mb-4">
-                        <button class="btn btn-primary hkta-primary-btn" id="JAR4" type="button">JAR4
-                        </button>
-                    </div>
-                    <div class="col-xl-3 col-md-4 col-6 mb-4">
-                        <button class="btn btn-primary hkta-primary-btn" id="STZ4" type="button">STZ4
-                        </button>
-                    </div>
-                    <div class="col-xl-3 col-md-4 col-6 mb-4">
-                        <button class="btn btn-primary hkta-primary-btn" id="DAL4" type="button">DAL4
-                        </button>
+            $total = (int)($sound_row / 4);
+            for ($i = 0; $i < $total; $i++) {
+                $j=$i*4;
+                ?>
+                <div class="col-lg-6">
+                    <div class="row">
+                        <div class="col-xl-3 col-md-4 col-6 mb-4">
+                            <button class="btn btn-primary hkta-primary-btn" id="<?php echo $sound_data[$j]['code']; ?>" type="button"><?php echo $sound_data[$j]['code']; ?></button>
+                        </div>
+                        <div class="col-xl-3 col-md-4 col-6 mb-4">
+                            <button class="btn btn-primary hkta-primary-btn" id="<?php echo $sound_data[$j+1]['code']; ?>" type="button"><?php echo $sound_data[$j+1]['code']; ?></button>
+                        </div>
+                        <div class="col-xl-3 col-md-4 col-6 mb-4">
+                            <button class="btn btn-primary hkta-primary-btn" id="<?php echo $sound_data[$j+2]['code']; ?>" type="button"><?php echo $sound_data[$j+2]['code']; ?></button>
+                        </div>
+                        <div class="col-xl-3 col-md-4 col-6 mb-4">
+                            <button class="btn btn-primary hkta-primary-btn" id="<?php echo $sound_data[$j+3]['code']; ?>" type="button"><?php echo $sound_data[$j+3]['code']; ?></button>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="container-fluid">
-        <div class="row text-center">
-            <div class="col-lg-6">
-                <div class="row">
-                    <div class="col-xl-3 col-md-4 col-6 mb-4">
-                        <button class="btn btn-primary hkta-primary-btn" id="KEJ5" type="button">KEJ5
-                        </button>
-                    </div>
-                    <div class="col-xl-3 col-md-4 col-6 mb-4">
-                        <button class="btn btn-primary hkta-primary-btn" id="ASK5" type="button">ASK5
-                        </button>
-                    </div>
-
-                    <div class="col-xl-3 col-md-4 col-6 mb-4">
-                        <button class="btn btn-primary hkta-primary-btn" id="AMC5" type="button">AMC5
-                        </button>
-                    </div>
-                    <div class="col-xl-3 col-md-4 col-6 mb-4">
-                        <button class="btn btn-primary hkta-primary-btn" id="ALV5" type="button">ALV5
-                        </button>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-6">
-                <div class="row">
-                    <div class="col-xl-3 col-md-4 col-6 mb-4">
-                        <button class="btn btn-primary hkta-primary-btn" id="CHV6" type="button">CHV6
-                        </button>
-                    </div>
-                    <div class="col-xl-3 col-md-4 col-6 mb-4">
-                        <button class="btn btn-primary hkta-primary-btn" id="JUL6" type="button">JUL6
-                        </button>
-                    </div>
-
-                    <div class="col-xl-3 col-md-4 col-6 mb-4">
-                        <button class="btn btn-primary hkta-primary-btn" id="MIL6" type="button">MIL6
-                        </button>
-                    </div>
-                    <div class="col-xl-3 col-md-4 col-6 mb-4">
-                        <button class="btn btn-primary hkta-primary-btn" id="BEB6" type="button">BEB6
-                        </button>
-                    </div>
-                </div>
-            </div>
+                <?php
+            }
+            ?>
         </div>
     </div>
 </section>
@@ -326,7 +230,7 @@ $_SESSION['user_id'] = $data[0]['id'];
 <script>
     $(document).ready(function () {
         //to disable the entire page
-        $("body").on("contextmenu",function(e){
+        $("body").on("contextmenu", function (e) {
             return false;
         });
 
@@ -339,22 +243,22 @@ $_SESSION['user_id'] = $data[0]['id'];
         document.onkeydown = function (e) {
 
             // disable F12 key
-            if(e.keyCode == 123) {
+            if (e.keyCode == 123) {
                 return false;
             }
 
             // disable I key
-            if(e.ctrlKey && e.shiftKey && e.keyCode == 73){
+            if (e.ctrlKey && e.shiftKey && e.keyCode == 73) {
                 return false;
             }
 
             // disable J key
-            if(e.ctrlKey && e.shiftKey && e.keyCode == 74) {
+            if (e.ctrlKey && e.shiftKey && e.keyCode == 74) {
                 return false;
             }
 
             // disable U key
-            if(e.ctrlKey && e.keyCode == 85) {
+            if (e.ctrlKey && e.keyCode == 85) {
                 return false;
             }
         }
